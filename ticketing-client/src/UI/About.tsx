8b8ba23a75ee40a0ea07ec9dbf17d4d241c27065
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-03-11 20:15:20 +0200

    same files

---
+++ ticketing-client/src/UI/About.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function About(){
+
+    return (
+
+        <div>
+            <h1>About Ticketing App</h1>
+            <p>
+                Some details about the app and so on
+            </p>
+
+            <p>
+                More details about the app and so on
+            </p>
+
+            <p>
+                &copy; TO
+            </p>
+        </div>
+    )
+}
---
+++ ticketing-client/src/UI/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './home.css'
+import splash from './homeimage.jpg'
+
+export default function Home(){
+    <div className="container">
+        <img src={splash} alt="Ticketing App"></img>
+        <div className="centered">Ticketing App</div>
+    </div>
+}
