6241989098506844d36893b3b9a8595821e7a0b0
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-03-31 18:34:52 +0300

    asda

---
+++ ticketing-client/src/UI/MyLocations.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import RestClient from '../REST/RestClient';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Location } from '../interfaces/Location';
+import splash from "./homeimage.jpg";
+
+export default function MyLocations() {
+  const [locations, setLocations] = useState<Location[]>([]);
+  
+
+  const handleDemo1 = () => {
+    const promise = RestClient.getLocations()
+    promise.then(data => setLocations(data));
+  }
+
+  function handleDemo2(id: number) {
+    const promise = RestClient.getOneLocation(id)
+    promise.then(data => console.log(`Location1: ${JSON.stringify(data)}`))
+  }
+
+  return (
+    <div className="container">
+      <h1>My Locations</h1>
+      <div>Click a button to call a REST API</div>
+      <button onClick={handleDemo1}>Get all locations</button>
+      <button onClick={() => handleDemo2(1)}>Location1</button>
+      <div className="row">
+        {locations.map((location, index) => {
+          return (
+            <div key={index} className="col-2 mb-3">
+              <img src={splash} alt="location" width="145" height="201" />
+              <p className="text-center">{location.place}</p>
+            </div>
+          )
+        })}
+        {Array(22 - locations.length).fill(null).map((_, index) => {
+          return (
+            <div key={index + locations.length} className="col-2 mb-3">
+              <img src={splash} alt="location" width="145" height="201"/>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ ticketing-client/src/interfaces/Location.ts
@@ -0,0 +1,5 @@
+export interface Location {
+    id: number;
+    place: string;
+    capacity: number;
+  }  
