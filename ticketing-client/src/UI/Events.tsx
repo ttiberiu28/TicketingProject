8571d01c88cacc309fbc9c3902ef3cf972ae05f9
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-04-05 00:34:34 +0300

    added more events on the page with scroll down option

--- ticketing-client/src/UI/Events.tsx
+++ ticketing-client/src/UI/Events.tsx
@@ -5,80 +5,44 @@ import './Events.css';
 import myImage from './Images/im4.jpg';
 import { Link } from 'react-router-dom';
 
-
 interface Movie {
   id: number;
   name: string;
   imdbRating: number;
-  // Add more properties as needed
 }
 
 interface StandUp {
   id: number;
   name: string;
-  // Add more properties as needed
 }
 
-export default function Events(){
-
+export default function Events() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [standUpEvents, setStandUpEvents] = useState<StandUp[]>([]);
 
   useEffect(() => {
-    getMovies().then(data => setMovies(data));
-    getStandUpEvents().then(data => setStandUpEvents(data));
+    getMovies().then((data) => setMovies(data));
+    getStandUpEvents().then((data) => setStandUpEvents(data));
   }, []);
 
   return (
-    
     <Container className="movies-and-standups">
-      <Row>
-        <Col>
-          <h2 className="section-title">Movies</h2>
-          <Row>
-            {movies.map(movie => (
-              <Col md={2} key={movie.id}>
+      <Row className="justify-content-center align-items-center vh-100">
+        <Col className="text-center">
+          <h2 className="section-title mb-5">Movies</h2>
+          <Row className="justify-content-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24].map((index) => (
+              <Col md={2} key={index} className="mb-4">
                 <Card className="event-card">
-                <Card.Img variant="top" src={myImage} />
-                <Card.Body>
-                    <Card.Title>{movie.name}</Card.Title>
-                    <Card.Text>Some description for the movie</Card.Text>
-                </Card.Body>
-                </Card>
-                
-              </Col>
-            ))}
-          </Row>
-        </Col>
-        <Col>
-          <Row>
-            {standUpEvents.map(event => (
-              <Col md={2} key={event.id}>
-                <Card className="event-card">
-                <Card.Img variant="top" src={myImage} />
-                  <Card.Body>
-                    <Card.Title>{event.name}</Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Col>
-      </Row>
-      <Row className="my-5">
-        <Col>
-          <Row>
-            {[1, 2, 3, 4, 5].map(index => (
-              <Col md={2} key={index}>
-                <Card className="event-card">
-                    
-                <Link to={`/movie/${index}`}>
+                  <Link to={`/movie/${index}`}>
                     <Card.Img variant="top" src={myImage} />
-                </Link>
+                  </Link>
                   <Card.Body>
                     <Card.Title>Movie {index}</Card.Title>
                     <Card.Text>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.
                     </Card.Text>
                   </Card.Body>
                 </Card>
