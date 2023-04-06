8200b9386c9688692ee44830b49a64eda7c13311
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-04-06 20:16:11 +0300

    collapse add to cart buttons

--- ticketing-client/src/UI/MovieDetails.tsx
+++ ticketing-client/src/UI/MovieDetails.tsx
@@ -1,24 +1,86 @@
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { getMovies } from '../api/api';
+// import { Container, Row, Col, Card, Button, Collapse } from 'react-bootstrap';
+// import { Movie } from '../interfaces/Movie';
+// import './CSS/MovieDetails.css';
+
+// const MovieDetails: React.FC = () => {
+//   const { index = '0' } = useParams<{ index?: string }>();
+//   const [movie, setMovie] = useState<Movie | null>(null);
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const [showTickets, setShowTickets] = useState(false);
+
+//   useEffect(() => {
+//     getMovies(parseInt(index)).then((data) => setMovie(data[0]));
+//   }, [index]);
+
+//   if (!movie) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="movie-details-container">
+//       <Container>
+//         <Row>
+//           <Col xs={12} md={4}>
+//             <Card className="movie-poster">
+//               <Card.Img variant="top" src={movie.imageUrl} />
+//             </Card>
+//           </Col>
+//           <Col xs={12} md={8}>
+//             <div className="movie-description">
+//               <h1>{movie.name}</h1>
+//               <ul>
+//                 <li>IMDb Rating: {movie.imdbRating}</li>
+//                 <li>Duration: {movie.lengthMinutes} minutes</li>
+//                 <li>Language: {movie.language}</li>
+//               </ul>
+//               <p>{movie.movieDescription}</p>
+//               <Button variant="primary" onClick={() => setShowTickets(!showTickets)}>Buy tickets</Button>
+//               <Collapse in={showTickets}>
+//                 <div className="ticket-section">
+//                   <ul>
+//                     <li>2D: {movie.price} per ticket</li>
+//                     <li>3D: {movie.price + 8} per ticket</li>
+//                   </ul>
+//                 </div>
+//               </Collapse>
+//             </div>
+//           </Col>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// };
+
+// export default MovieDetails;
+
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovies } from '../api/api';
-import { Container, Row, Col, Card, Carousel, Button } from 'react-bootstrap';
-import { Movie } from '../interfaces/Movie'; // import the Movie interface
-import './CSS/MovieDetails.css'; // import custom CSS file
+import { Container, Row, Col, Card, Button, Collapse } from 'react-bootstrap';
+import { Movie } from '../interfaces/Movie';
+import './CSS/MovieDetails.css';
 
 const MovieDetails: React.FC = () => {
-  const { index = '0' } = useParams<{ index?: string }>(); // get the ID of the movie from the URL
-  const [movie, setMovie] = useState<Movie | null>(null); // create a state variable to hold the movie data
-  const [activeIndex, setActiveIndex] = useState(0); // create a state variable to track the active slide index
+  const { index = '0' } = useParams<{ index?: string }>();
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [showTickets, setShowTickets] = useState(false);
 
   useEffect(() => {
-    getMovies(parseInt(index)).then((data) => setMovie(data[0])); // fetch the movie data when the component mounts and set the state variable
-  }, [index]); // re-fetch the movie data whenever the ID in the URL changes
+    getMovies(parseInt(index)).then((data) => setMovie(data[0]));
+  }, [index]);
 
   if (!movie) {
-    return <div>Loading...</div>; // show a loading message until the movie data is fetched
+    return <div>Loading...</div>;
   }
 
-  // show the movie details once the data is fetched
+  const handleTicketClick = () => {
+    setShowTickets(!showTickets);
+  };
+
   return (
     <div className="movie-details-container">
       <Container>
@@ -37,7 +99,32 @@ const MovieDetails: React.FC = () => {
                 <li>Language: {movie.language}</li>
               </ul>
               <p>{movie.movieDescription}</p>
-              <Button variant="primary">Buy tickets</Button>
+              <Button variant="primary" onClick={handleTicketClick}>
+                Buy tickets
+              </Button>
+              <Collapse in={showTickets}>
+              <div className="ticket-section">
+                <ul>
+                  <li>
+                    <span><b>2D:</b> {movie.price} RON</span>
+                    <Button variant="success" style={{fontWeight: 'bold', fontSize: '0.20rem'}}>Add to Cart</Button>
+                  </li>
+                  <li>
+                    <span><b>3D:</b> {movie.price + 8} RON</span>
+                    <Button variant="success" style={{fontWeight: 'bold', fontSize: '0.20rem'}}>Add to Cart</Button>
+                  </li>
+                  <li>
+                    <span><b>VIP 2D:</b> {movie.price + 40} RON</span>
+                    <Button variant="success" style={{fontWeight: 'bold', fontSize: '0.20rem'}}>Add to Cart</Button>
+                  </li>
+                  <li>
+                    <span><b>VIP 3D:</b> {movie.price + 48} RON</span>
+                    <Button variant="success" style={{fontWeight: 'bold', fontSize: '0.20rem'}}>Add to Cart</Button>
+                  </li>
+                </ul>
+              </div>
+
+              </Collapse>
             </div>
           </Col>
         </Row>
@@ -47,3 +134,4 @@ const MovieDetails: React.FC = () => {
 };
 
 export default MovieDetails;
+
