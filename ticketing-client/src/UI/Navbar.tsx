357ca0753cc1be6bb52f6e7e72fdafad67728277
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-04-07 18:18:16 +0300

    redesigned navbar

--- ticketing-client/src/UI/Navbar.tsx
+++ ticketing-client/src/UI/Navbar.tsx
@@ -1,35 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import homeIcon from './Images/im2.jpg';
-import { LoginProps } from '../interfaces/LoginProps'
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <img src={homeIcon} alt="home" width="30" height="30" className="d-inline-block align-text-top me-2" />
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/locations">Locations</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/events">Events</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav className="navbar navbar-expand-lg sticky-bottom navbar-dark bg-dark">
+
+              <Link className="navbar-brand" to="/">
+                <a className="navbar-brand" href="#">...............</a>
+                <a className="navbar-brand" href="#">...............</a>
+                <a className="navbar-brand" href="#">...............</a>
+              </Link>
+            
+              <Link className="navbar-brand" to="/">
+                <img src={homeIcon} alt="home" width="40" height="60" className="d-inline-block align-text-top me-2" />
+              </Link>
+            
+              <Link className="navbar-brand" to="/">
+                <a className="navbar-brand" href="#">Ticket-to-GO</a>
+              </Link>
+
+              <Link className="nav-link" to="/about">
+                <a className="navbar-brand" href="#">About</a>
+              </Link>
+            
+              <Link className="nav-link" to="/locations">
+                <a className="navbar-brand" href="#">Locations</a>
+              </Link>
+            
+              <Link className="nav-link" to="/events">
+                <a className="navbar-brand" href="#">Events</a>
+              </Link>
+            
+              <Link className="nav-link" to="/login">
+                <a className="navbar-brand" href="#">Login</a>
+              </Link>
+              
+            
+          
     </nav>
   );
 }
