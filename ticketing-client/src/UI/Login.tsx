ed3eec39fcfce8b7ff78830dade0d25b1be59b15
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-04-08 18:58:48 +0300

    added a new library that uses bootstrap elements

--- ticketing-client/src/UI/Login.tsx
+++ ticketing-client/src/UI/Login.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
 import RestClient from "../REST/RestClient";
-import "./CSS/login.css";
 import { useNavigate } from "react-router-dom";
-import "./CSS/CustomJumbotron.css"
-import { useAuth } from "./AuthContext"
+import { useAuth } from "./AuthContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 // define the Props interface for the Login component
 interface Props {
   onLogin: () => void; // callback to trigger after successful login
@@ -42,48 +42,117 @@ const Login: React.FC<Props> = ({ onLogin }) => {
   };
 
   return (
-    <div className="login-page jumbotron jumbotron-fluid custom-jumbotron">
-      <Form>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            value={username}
-            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) =>
-              setUsername(e.target.value)
-            }
-          />
-        </Form.Group>
+    <section className="vh-100" style={{ backgroundColor: "#9A616D" }}>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10">
+            <div className="card" style={{ borderRadius: "1rem" }}>
+              <div className="row g-0">
+                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                  <img
+                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                    alt="login form"
+                    className="img-fluid"
+                    style={{ borderRadius: "1rem 0 0 1rem" }}
+                  />
+                </div>
+                <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                  <div className="card-body p-4 p-lg-5 text-black">
+                    <form>
+                      <div className="d-flex align-items-center mb-3 pb-1">
+                        <i
+                          className="fas fa-duotone fa-mountain-sun fa-2x me-3"
+                          style={{ color: "#ff6219" }}
+                        ></i>
+                        <span className="h1 fw-bold mb-0">Ticket-to-GO</span>
+                      </div>
 
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e: { target: { value: React.SetStateAction<string>; }; }) =>
-              setPassword(e.target.value)
-            }
-          />
-        </Form.Group>
+                      <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: "1px" }}>
+                        Sign into your account
+                      </h5>
 
-        <Button variant="primary" type="button" onClick={handleLogin}>
-          Login
-        </Button>
-        <Button type="button" onClick={() => navigate('/signup')}>
-          Create an account
-        </Button>
+                      <div className="form-outline mb-4">
+                        <input
+                          type="text"
+                          id="form2Example17"
+                          className="form-control form-control-lg"
+                          value={username}
+                          onChange={(e) =>
+                            setUsername(e.target.value)
+                          }
+                        />
+                        <label className="form-label" htmlFor="form2Example17">
+                          Username
+                        </label>
+                      </div>
 
-        {loginStatus === "success" && (
-          <p className="login-suc lead">Login successful!</p>
-        )}
-        {loginStatus === "failure" && (
-          <p className="text-danger lead ">Incorrect username or password</p>
-        )}
-      </Form>
-    </div>
-  );
-};
-
-export default Login;
+                      <div className="form-outline mb-4">
+                        <input
+                          type="password"
+                          id="form2Example27"
+                          className="form-control form-control-lg"
+                          value={password}
+                          onChange={(e) =>
+                            setPassword(e.target.value)
+                          }
+                        />
+                        <label className="form-label" htmlFor="form2Example27">
+                          Password
+                        </label>
+                      </div>
+    
+                      <div className="pt-1 mb-4">
+                        <button
+                          className="btn btn-dark btn-lg btn-block"
+                          type="button"
+                          onClick={handleLogin}
+                        >
+                          Login
+                        </button>
+                      </div>
+    
+                      <a className="small text-muted" href="#!">
+                        Forgot password?
+                      </a>
+                      <p
+                        className="mb-5 pb-lg-2"
+                        style={{ color: "#393f81" }}
+                      >
+                        {/* Go to /signup to create account */}
+                        Don't have an account?{" "}
+                        <a
+                          href="#!"
+                          onClick={() => navigate("/signup")}
+                          style={{ color: "#393f81" }}
+                        >
+                          Register here
+                        </a>
+                      </p>
+                      <a href="#!" className="small text-muted">
+                        Terms of use.
+                      </a>
+                      <a href="#!" className="small text-muted">
+                        Privacy policy
+                      </a>
+    
+                      {loginStatus === "success" && (
+                        <p className="login-suc lead">Login successful!</p>
+                      )}
+                      {loginStatus === "failure" && (
+                        <p className="text-danger lead ">
+                          Incorrect username or password
+                        </p>
+                      )}
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    );
+  };
+  
+  export default Login;
