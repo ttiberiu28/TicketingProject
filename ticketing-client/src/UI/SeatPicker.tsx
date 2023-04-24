427552819017d3c60553481c2cc617c974ddb84d
Ordean Tiberiu <ttiberiu28@yahoo.com>
2023-04-24 21:45:02 +0300

    seat picker design

--- ticketing-client/src/UI/SeatPicker.tsx
+++ ticketing-client/src/UI/SeatPicker.tsx
@@ -2,11 +2,10 @@
 import React from 'react';
 
 interface CustomSeatPickerProps {
-    seatsLayout: number[][];
+    seatsLayout: (number | null)[][];
     onSeatSelected: (row: number, seat: number) => void;
-    selectedSeats: { row: number; seat: number }[]; // Add this line
+    selectedSeats: { row: number; seat: number }[];
 }
-
 
 export const CustomSeatPicker: React.FC<CustomSeatPickerProps> = ({ seatsLayout, onSeatSelected, selectedSeats }) => {
     const handleSeatClick = (row: number, seat: number) => {
@@ -19,17 +18,21 @@ export const CustomSeatPicker: React.FC<CustomSeatPickerProps> = ({ seatsLayout,
             {seatsLayout.map((rowSeats, rowIndex) => (
                 <div key={`row-${rowIndex}`} className="row">
                     {rowSeats.map((seatNumber, seatIndex) => (
-                        <button
-                            key={`seat-${seatNumber}`}
-                            className={`seat ${selectedSeats.some((s: { row: number; seat: number; }) => s.row === rowIndex + 1 && s.seat === seatNumber) ? 'selected' : ''}`}
-                            onClick={() => handleSeatClick(rowIndex + 1, seatNumber)}
-                        >
-                            {seatNumber}
-                        </button>
+                        seatNumber !== null ? (
+                            <button
+                                key={`seat-${seatNumber}`}
+                                className={`seat ${selectedSeats.some((s: { row: number; seat: number; }) =>
+                                    s.row === rowIndex + 1 && s.seat === seatNumber) ? 'selected' : ''}`}
+                                onClick={() => handleSeatClick(rowIndex + 1, seatNumber)}
+                            >
+                                {seatNumber}
+                            </button>
+                        ) : (
+                            <div key={`empty-${seatIndex}`} style={{ width: '40px', height: '30px', margin: '3px' }}></div> // Render an empty div for the white space
+                        )
                     ))}
                 </div>
             ))}
         </div>
     );
 };
-
